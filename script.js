214990a51173a40eb31d51d319b7a7f7703efc1b
Mark <88643636+marrrkkk@users.noreply.github.com>
2023-07-20 09:29:16 +0800

    🚀First commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const front = document.getElementById('front')
+    const click = document.getElementById('click')
+    const body = document.documentElement
+    const enterFullscreen = () => {
+        const element = document.documentElement;
+        if (element.requestFullscreen) {
+          element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) { // Firefox
+          element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+          element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) { // IE/Edge
+          element.msRequestFullscreen();
+        }
+      }
+
+    front.addEventListener('click', e => {
+      body.style.cursor = "none"
+      click.style.opacity = "0";
+        enterFullscreen()
+        setTimeout(() => {
+            front.remove()
+        }, 5000)
+
+        setTimeout(() => {
+          const counter = document.getElementById('counter')
+          let percent = 1
+      
+        const interval = setInterval(() => {
+              percent++
+              counter.innerText = percent
+              if(percent === 100){
+                clearInterval(interval)
+                return alert("wow you actually finish this💀💀💀")
+              }
+          }, 10000)
+        }, 5000)
+    })
+})
